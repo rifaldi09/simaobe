9fc9a6ca629f0c0c77f0cb8847205e756ae0861c
rezibos <rezibos>
2024-10-20 22:10:17 +0700

    update

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Landing Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('resources/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets_login/css/style.css') }}">
 </head>
 <body>
     <header>
