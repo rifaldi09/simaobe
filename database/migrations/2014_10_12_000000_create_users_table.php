3a2e8a765b1776f39232ab95399f67a9a41a7ce0
fyan <sapigamimg@gmail.com>
2024-10-20 21:30:18 +0700

    tabel user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,12 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->id('UserID')->increment();
+            $table->string('UserEmail')->unique();
+            $table->string('UserName')->unique();
+            $table->string('Password')->unique();
+            $table->integer('RoleID');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
