e22aeeaf54d1fcd3f2b68784586c64ae5193399d
Kika Haekal <91299160+Kikahaekal@users.noreply.github.com>
2024-11-09 12:31:47 +0700

    tambah header basis evaluasi pada folder component

--- resources/views/landing_page/basis evaluasi/penilaian-sub-cpmk.blade.php
+++ resources/views/landing_page/basis evaluasi/penilaian-sub-cpmk.blade.php
@@ -1,23 +1,6 @@
 @extends('layout.main')
 @section('content')
-    {{--! Bagian Navbar --}}
-    <header>
-        <div class="navbar navbar-dark shadow-sm">
-            <div class="container d-flex flex-column flex-md-row justify-content-between">
-                <div class="d-flex align-items-center col-12 col-md-3 container">
-                    <div>
-                        <span class="title">Sistem Manajemen</span>
-                        <span class="subtitle">Kurikulum OBE</span>
-                    </div>
-                </div>
-
-                <div class="container d-flex align-items-center col-md-9 text-center mt-3 mt-md-0">
-                    <p class="text-white display-6 fw-bold">Basis Evaluasi Penilaian</p>
-                </div>
-            </div>
-        </div>
-    </header>
-
+@include('landing_page.basis evaluasi.components.header')
     <main>
         <div class="container mt-3">
             <h2>Rubrik Penilaian SUB CPMK</h2>
@@ -25,7 +8,11 @@
         <hr class="border border-2 border-dark">
         <div class="container">
             <h3>Sub CPMK</h3>
-            <select name="subcpmk" id="subcpmk"></select>
+            <select name="subcpmk" id="subcpmk" class="form-control w-50">
+                <option hidden class="text-muted">--No Selection--</option>
+                <option value="">Sub CPMK1 - Lorem ipsum dolor sit.</option>
+                <option value="">Sub CPMK2 - Lorem ipsum dolor sit.</option>
+            </select>
         </div>
     </main>
 @endsection
