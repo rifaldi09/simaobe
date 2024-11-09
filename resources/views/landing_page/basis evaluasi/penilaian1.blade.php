21d2ffb13061a186e800a80d10adfa678b8d1b42
FaizCukip <muhammadfaiuhu@gmail.com>
2024-11-09 13:19:49 +0700

    "penilaian"

--- resources/views/landing_page/basis evaluasi/penilaian1.blade.php
+++ resources/views/landing_page/basis evaluasi/penilaian1.blade.php
@@ -2,6 +2,22 @@
 @section('content')
 @include('landing_page.basis evaluasi.components.header')
 
-
+<main>
+  <div class="container mt-3">
+    <h2>&nbsp;</h2>
+  </div>
+  <hr class="border border-2 border-dark">
+  <div class="d-flex justify-content-center">
+    <div class="card w-75">
+      <div class="card-body">
+        <h5 class="card-title">Interaksi Manusia dan Komputer</h5>
+        <br>
+        <p class="card-text">INF11118</p>
+        <h5 class="card-title">2 SKS</h5>
+        <h5 class="card-title">Teknik Informatika</h5>
+      </div>
+    </div>
+  </div>
+</main>
 
 @endsection
