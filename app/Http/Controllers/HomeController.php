57f6248df7ded1e64f9c5fec60ae9216b6231eb9
FaizCukip <muhammadfaiuhu@gmail.com>
2024-12-04 15:02:35 +0700

    Tambah Logika Analisis

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Analisis;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -22,9 +23,13 @@ class HomeController extends Controller
     // menampilkan halaman utama analisis pembelajaran
     public function analisis_main_page()
     {
+        $session = [
+            "sessionID" => session('sessionID')
+        ];
+        $data = Analisis::getAnalisis($session);
         return view('landing_page.analisis-mata-kuliah', [
             'title' => 'Analisis Pembelajaran Page'
-        ]);
+        ], compact('data'));
     }
 
     // menampilkan halaman utama basis evaluasi pembelajaran
--- app/Models/Analisis.php
+++ app/Models/Analisis.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
 
 class Analisis extends Model
 {
     use HasFactory;
+
+    static function path($param = null)
+    {
+        return "http://148.135.137.186:54312/" . $param;
+    }
+
+    static function getAnalisis($sessionId)
+    {
+        $response = Http::post(self::path("aap/get"), $sessionId);
+        return $response->json();
+    }
 }
