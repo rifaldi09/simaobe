f8535bd7f38f4cfa46de08bff7ad8c254083727a
Kika Haekal <91299160+Kikahaekal@users.noreply.github.com>
2024-12-07 22:13:14 +0700

    tambah logika analisis

--- resources/views/landing_page/analisis.blade.php
+++ resources/views/landing_page/analisis.blade.php
@@ -68,16 +68,16 @@
             <div class="container mt-1 justify-content-center">
                 <p class="h3">CPMK</p>
                 <select id="selectMultipleCPMK1" class="form-control" name="analisis[0][cpmk][]" multiple="multiple">
-                        @foreach ($analisis as $respon)
+                        {{-- @foreach ($analisis as $respon)
                             <option value="{{ $respon['CPMKID'] }}">{{ $respon['KetCPMK'] }}</option>
-                        @endforeach
+                        @endforeach --}}    
                 </select>
             </div>
 
 
-            <div class="container mt-1 justify-content-center m-3">
-                <p class="h3">Model Pembelajaran</p>
-                <select class="form-select">
+            <div class="container mt-1 justify-content-center">
+                <p class="h3">CPL</p>
+                <select class="form-select" name="analisis[0][cplid]" onchange="onCpmkChange()" id="selectCpl1">
                     @foreach ($cpl as $respon)
                         <option value="{{ $respon['CPLID'] }}">{{ $respon['KetCPL'] }}</option>
                     @endforeach
@@ -87,7 +87,7 @@
 
             <div class="container mt-1 justify-content-center mt-3">
                 <p class="h3">KodeSubCpmk</p>
-                <textarea id="kscmpk" class="form-control"></textarea>
+                <textarea id="kscmpk" class="form-control" name="kscpmk"></textarea>
             </div>
 
 
