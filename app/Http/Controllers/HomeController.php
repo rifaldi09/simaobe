f963eeeb11484f282f3f33a41afcb0f8c45324b1
SpyHolder <ikhsansadillah@gmail.com>
2025-02-13 22:19:46 +0700

    laravel excel

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -221,6 +221,7 @@ class HomeController extends Controller
         
         $data = KelasNilaiMKDosen::getNilaiMK($session);
         $dataP = [];
+        $dataR = [];
 
         // dd($data);
         foreach ($data[0] as $key => $value) {
@@ -229,10 +230,15 @@ class HomeController extends Controller
                 $cpmkKey = explode('_',$key)[1];
                 $dataP[$baseKey][] = $cpmkKey;
             }
+            if (strpos($key,"CPMK") !== false) {
+                $baseKey = explode('_',$key)[0];
+                $cpmkKey = explode('_', $value)[0];
+                $dataR[$baseKey][] = $cpmkKey;
+            }
         }
-
+        // dd($dataR);
         return view('nilaiKelasMKS', [
             'title' => 'Nilai Kelas Matkul'
-        ], compact('data','dataP'));
+        ], compact('data','dataP','dataR'));
     }
 }
--- resources/views/nilaiKelasMKS.blade.php
+++ resources/views/nilaiKelasMKS.blade.php
@@ -6,42 +6,66 @@
         <h1 class="fw-bold font-outfit">Table Nilai Mata Kuliah</h1>
     </div>
     <div class="tabs-bg float-start py-2 w-100 mb-3">
-        
     </div>
+    <div class="clearfix"></div>
 </header>
 
 <main>
-    
-    <table class="table">
-        <tr class="text-center">
-            <th rowspan="2">No</th>
-            <th rowspan="2">NIM</th>
-            <th rowspan="2">Nama Mahasiswa</th>
-            @foreach ($dataP as $key => $value)
-                <th colspan="{{ count($value) }}">{{ $key }}</th>
-            @endforeach
-        </tr>
-    
-        <tr class="text-center">
-            @foreach ($dataP as $key => $value)
-                @foreach ($value as $cpmk)
-                    <th>{{ $cpmk }}</th>
-                @endforeach
-            @endforeach
-        </tr>
-        @foreach ($data as $key => $value)
-        <tr class="text-center">
-                <td>{{ ++$key }}</td>
-                <td>{{ $value["NIM"] }}</td>
-                <td>{{ $value["Mahasiswa"] }}</td>
-                @foreach ($dataP as $key => $item)
-                    @foreach ($item as $cpmk)
-                        <td>{{ $value[$key . '_' . $cpmk] ?? '-' }}</td>
+        <div class="container">
+            <div class="d-flex justify-content-between mb-2">
+                    <form action="" method="post" class="d-flex" enctype="multipart/form-data">
+                        <input type="file" class="form-control">
+                        <button class="btn btn-primary align-self-start ms-2">Upload</button>
+                    </form>
+                <div class="d-flex">
+                    <form action="" method="post" enctype="multipart/form-data">
+                        <button class="btn btn-success mb-2">Unduh</button>
+                    </form>
+                    <form action="" method="post">
+                        <button class="btn btn-danger ms-2">Print</button>
+                    </form>
+                </div>
+            </div>
+            <table class=" table table-bordered" style="font-size:12px">
+                <tr class="text-center">
+                    <th rowspan="2">No</th>
+                    <th rowspan="2">NIM</th>
+                    <th rowspan="2">Nama Mahasiswa</th>
+                    @foreach ($dataP as $key => $value)
+                        <th colspan="{{ count($value) }}">{{ $key }}</th>
                     @endforeach
+                    @foreach ($dataP as $key => $value)
+                        <th rowspan="2">{{ $key }}</th>
+                    @endforeach
+                    <th rowspan="2">Grade</th>
+                </tr>
+            
+                <tr class="text-center">
+                    @foreach ($dataP as $key => $value)
+                        @foreach ($value as $cpmk)
+                            <th>{{ $cpmk }}</th>
+                        @endforeach
+                    @endforeach
+                </tr>
+                @foreach ($data as $key => $value)
+                <tr>
+                        <td>{{ ++$key }}</td>
+                        <td>{{ $value["NIM"] }}</td>
+                        <td>{{ $value["Mahasiswa"] }}</td>
+                        @foreach ($dataP as $key => $item)
+                            @foreach ($item as $cpmk)
+                                <td class="text-center">{{ $value[$key . '_' . $cpmk] ?? '0' }}</td>
+                            @endforeach
+                        @endforeach
+                        {{-- @foreach ($dataR as $key => $value)
+                        @foreach ($value as $data)
+                            <td>{{ $data }}</td>
+                        @endforeach
+                        @endforeach --}}
+                    </tr>
                 @endforeach
-            </tr>
-        @endforeach
-    </table>
+            </table>
+        </div>
 </main>
 
 
